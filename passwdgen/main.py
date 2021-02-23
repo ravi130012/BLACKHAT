bbd22ba3e30e00e3e9e1b4abcdec79c044a81e95
ravi130012 <65115218+ravi130012@users.noreply.github.com>
2021-02-23 19:29:33 +0530

    Delete passwdgen directory

--- passwdgen/main.py
+++
@@ -1,9 +0,0 @@
-import os
-try:
-    os.system("cp pass.sh /bin/passgen")
-    
-    print("autofile created!")
-    print('''type "passgen" from an directory to access password generator.''')
-except Exception as e:
-    print(e)
-    print("cannot create the autofile")
--- passwdgen/pass.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-cd ~/Desktop/passgen 
-./passgen.py
